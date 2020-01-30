004c3e8e82e700fb90544b3f6ebe285bbab50bac
Felipe Nascimento Santos <felipe.santos@previd.lan>
2020-01-30 11:45:03 -0300

    import dashboard

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href='https://github.com/santosfelip' target='_blank'> Felipe Nascimento</a>.
+        </strong>
+    </footer>
+)
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,22 @@
+//Template
+import '../common/template/dependencies'
+
+import React from 'react'
+
+//Components
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+
+import Routes from './routes'
+
+export default props => (
+    <div className='wrapper'>
+        <Header />
+        <Sidebar />
+        <div className='content-wrapper'>
+            <Routes />
+        </div>
+        <Footer />
+    </div>
+)
