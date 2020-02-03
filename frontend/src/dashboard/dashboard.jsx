5ff4a8834e58619a6f7d05e062d6223ffadd7e2e
Felipe Nascimento <nascimento.fe14@gmail.com>
2020-02-03 19:23:49 -0400

    import front

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 //Components
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
-
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
@@ -11,7 +12,29 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox 
+                            cols='12 4'
+                            color='green'
+                            icon='bank'
+                            value='R$ 10'
+                            text='Total de Créditos'
+                        />
+                        <ValueBox 
+                            cols='12 4'
+                            color='red'
+                            icon='credit-card'
+                            value='R$ 10'
+                            text='Total de Débitos'
+                        />
+                        <ValueBox 
+                            cols='12 4'
+                            color='blue'
+                            icon='money'
+                            value='R$ 0'
+                            text='Valor consolidado'
+                        />
+                    </Row>
                 </Content>
             </div>
         )
